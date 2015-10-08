6d648e4c0f98a7949ab3de8982e14e97a07ee5be
CJ Gardner <omgborg@gmail.com>
2015-10-08 15:01:02 -0400

    Added some utility functions (connect db and is admin)

--- www/util.php
+++ www/util.php
@@ -1,5 +1,5 @@
 <?php
-
+include 'config.php';
 # Utitlity functions for the site
 
 # gets the currently logged in user
@@ -20,6 +20,29 @@ function set_user($username)
 function is_logged_in()
 {
 	return isset($_COOKIE["session"]);
+}
+
+# opens a db connection
+function connect_db()
+{
+	mysql_connect("$dbhost", "$dbuser", "$dbpass") or die("cannot connect");
+	mysql_select_db("$dbname") or die("cannot select db");
+}
+
+# checks if admin
+# must call connect_db first
+function is_admin($username)
+{
+	$u = mysql_real_escape_string($username);
+	$q = mysql_query("SELECT * FROM users WHERE username='$u'");
+	if($row = mysql_fetch_assoc($q))
+	{
+		if($row['is_admin'] == 't')
+		{
+			return true;
+		}
+	}
+	return false;
 }
 
 ?>
