42d2be537384ec55a533baf7842cb4678654718b
Henry Budris <hbudris1@umbc.edu>
2022-11-08 21:12:54 -0500

    Added wait for database in setup script

--- inc/setup.sh
+++ inc/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+wait_for_db() { mysqladmin -u root -h localhost ping; }
+
+until wait_for_db; do
+    :
+done
+
 mysqladmin -u root -h localhost password 5364ea964e8784e1c35ffb651acc6780
 mysqladmin -u root -h localhost --password="5364ea964e8784e1c35ffb651acc6780" create bates
 
