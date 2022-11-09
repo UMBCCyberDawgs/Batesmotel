26ea542ff7f501e77788588c5104b8b4bf3d9900
Henry Budris <hbudris1@umbc.edu>
2022-11-08 23:24:56 -0500

    Fixed typo in setup script

--- inc/setup.sh
+++ inc/setup.sh
@@ -12,10 +12,10 @@ mysqladmin -u root -h localhost --password="5364ea964e8784e1c35ffb651acc6780" cr
 sed -i.bak 's/^\(\$dbpass\ =\ \).*/\1\"5364ea964e8784e1c35ffb651acc6780\"\;/' /var/www/html/config.php
 
 if [ -f "/var/www/html/install.php" ]; then
-    echo "install.php file missing. Redownload this file and curl it if it hasn't already been"
-else
     curl http://localhost/install.php
     rm /var/www/html/install.php
+else
+    echo "install.php file missing. Redownload this file and curl it if it hasn't already been"
 fi
 
 echo "Please run: rm /tmp/setup.sh"
