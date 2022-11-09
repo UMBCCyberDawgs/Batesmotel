e7eea424f8b75ff34e0ca4941a4791327bff8491
Henry Budris <hbudris1@umbc.edu>
2022-11-08 22:14:40 -0500

    Testing store result in mysqli

--- www/lookup.php
+++ www/lookup.php
@@ -68,6 +68,7 @@ if(isset($_GET["username"]))
 	# lookup user
 	$user = $_GET["username"];
 	$q = mysqli_query($mysqli, "SELECT * FROM users WHERE username='$user'");
+  mysqli_stmt_store_result($q);
 	# since there might be multiple users with the same username, just dump all of them
 	$count = mysqli_stmt_num_rows($q);
 	if($count > 0)
