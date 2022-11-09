cab3b658af8e6b397c7ce5a0b54cf0ef261126fc
Henry Budris <hbudris1@umbc.edu>
2022-11-08 21:19:32 -0500

    Fixed some run and setup items

--- www/install.php
+++ www/install.php
@@ -7,7 +7,7 @@ $mysqli = connect_db();
 #mysqli_query("DROP TABLE users");
 
 # create table
-mysqli_query("CREATE TABLE users (
+mysqli_query($mysqli, "CREATE TABLE users (
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	username VARCHAR(100) NOT NULL,
 	password VARCHAR(100) NOT NULL,
