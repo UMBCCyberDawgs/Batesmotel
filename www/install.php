70c41735dfcb3e819d30267e3b16cf7d4e76c0ae
William Brooks <wbrooks1@umbc.edu>
2024-08-27 17:49:42 -0400

    Readded install.php

---
+++ www/install.php
@@ -0,0 +1,30 @@
+<?php
+include 'config.php';
+include 'util.php';
+$mysqli = connect_db();
+
+# drop table
+#mysqli_query("DROP TABLE users");
+
+# create table
+mysqli_query($mysqli, "CREATE TABLE users (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(100) NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	name VARCHAR(100),
+	about VARCHAR(500),
+	is_admin VARCHAR(10))");
+
+function reg_user($mysqli, $username, $password, $name, $about, $is_admin)
+{
+	$p = encrypt_password($password);
+	mysqli_query($mysqli, "INSERT INTO users (username, password, name, about, is_admin) VALUES('$username', '$p', '$name', '$about', '$is_admin')");
+}
+
+reg_user($mysqli, 'n0rmanbates', 'SuperDuperSecureSqordfish0!', 'Norman Bates', 'Hello! I own this motel!', 't');
+reg_user($mysqli, 'mcrane', 'helpmeplsaaaaaaaaaaa', 'Marion Crane', 'Just an average guest, nothing to see here', 'f');
+reg_user($mysqli, 'loomyboy1995', 'sex', 'Sam Loomis', 'Have you seen my girlfriend?', 'f');
+reg_user($mysqli, 'arbogast', 'trustno1notevenurself', 'Arbogast', 'PI for hire', 'f');
+reg_user($mysqli, 'lovelylila', 'hihihihihihihihi', 'Lila Crane', 'Do I have to put something here?', 'f');
+reg_user($mysqli, 'Norma', 'imthebestimnumber1', 'Norma', 'Watching over this place since 1925', 'f');
+?>
